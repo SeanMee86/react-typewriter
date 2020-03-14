0be6440f4f24026e40a19e45973cffe1ab20b9ec
SeanMee86 <sean.mee86@yahoo.com>
2020-03-14 09:00:43 -0700

    converted to class

--- typewriter.js
+++ typewriter.js
@@ -1,30 +1,30 @@
 import React, {useState} from "react";
 
-const TypeWriter = function(array, timeDelay) {
-    if(!array.length) array = [array];
-    this.newLine = '';
-    this.counter = 0;
-    this.arrayCounter = 1;
-    this.linesArray = array.map(el => el.props !== undefined ? el.props.children : el);
-    this.charsArray = this.linesArray.map(line => line.split(''));
+class TypeWriter{
+    constructor(array, timeDelay){
+        !Array.isArray(array) ? this.array = [array] : this.array = array;
+        this.timeDelay = timeDelay;
+        this.newLine = '';
+        this.counter = 0;
+        this.arrayCounter = 1;
+        this.linesArray = this.array.map(el => el.props !== undefined ? el.props.children : el);
+        this.charsArray = this.linesArray.map(line => line.split(''));
 
-    this.textObj = {};
-    this.charsArray.forEach((line, ind) => this.textObj[`line${ind+1}`] = '');
-    const [text, setText] = useState(this.textObj);
+        this.textObj = {};
+        this.charsArray.forEach((line, ind) => this.textObj[`line${ind+1}`] = '');
+        [this.text, this.setText] = useState(this.textObj);
+    }
 
-    this.createLines = () => {
+    createLines = () => {
         this.typingInterval = setInterval(() => {
             this.newLine = this.newLine + this.charsArray[this.arrayCounter - 1][this.counter];
-
-            setText((prevState) => {
+            this.setText((prevState) => {
                 return {
                     ...prevState,
                     [`line${this.arrayCounter}`]: this.newLine
                 }
             });
-
             this.counter++;
-
             if (this.counter >= this.charsArray[this.arrayCounter - 1].length) {
                 clearInterval(this.typingInterval);
                 if(this.arrayCounter < this.charsArray.length) {
@@ -34,26 +34,20 @@ const TypeWriter = function(array, timeDelay) {
                     this.createLines();
                 }
             }
-        }, timeDelay);
+        }, this.timeDelay);
     };
 
-    this.printText = () => {
-        return text;
-    };
+    printText = () => this.text;
 
-    this.typeLines = () => {
-        return this.charsArray
-            .map((line,ind) => {
-                const CustomTag = `${array[ind].type}`;
-                return (
-                    <CustomTag key={ind}>{this.printText()[`line${ind+1}`]}</CustomTag>
-                )
-            });
-    };
+    typeLines = () => this.charsArray
+        .map((line,ind) => {
+            const CustomTag = `${this.array[ind].type ? this.array[ind].type : 'p'}`;
+            return (
+                <CustomTag key={ind}>{this.printText()[`line${ind+1}`]}</CustomTag>
+            )
+        });
 
-    this.intervalRef = () => {
-        return this.typingInterval
-    };
-};
+    intervalRef = () => this.typingInterval
+}
 
 export default TypeWriter;
